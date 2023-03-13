4a107ce1316964d1937bbbc9e303e998943504d6
Mateus Drumond <mateusdrumondicm@gmail.com>
2023-03-13 19:28:45 -0300

    criacao do butao e lista de convidados

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 
 import { Container } from "./styles";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera do Ignite",
+    "Amigos",
+  ]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
